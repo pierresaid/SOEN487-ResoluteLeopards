898ee493fbef0cb659c0390dd4c030ee0f64de8d
PEAU Clement <clement.peau@epitech.eu>
2019-04-03 18:40:15 -0400

    Added a comment in the vote's model to make it clearer

--- post_api/post_models/vote.py
+++ post_api/post_models/vote.py
@@ -4,6 +4,7 @@ from main import db
 class Vote(db.Model):
     post_id = db.Column(db.Integer, nullable=False, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
+    # -1 no vote, 0 la premiere image, 1 la seconde
     value = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
