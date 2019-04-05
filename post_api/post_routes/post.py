1cc2d483deb0b767fd8dde9cb5c50366d99dfc4e
Clement Peau <clement.peau@gmail.com>
2019-04-05 00:18:29 -0400

    Modified the Vote route to add a bite more informations, still working on the user name and the call to the other microservices

--- post_api/post_routes/post.py
+++ post_api/post_routes/post.py
@@ -1,27 +1,37 @@
 from flask import Blueprint, request, jsonify, make_response
-from post_models import Post
+from post_models import Post, Vote
 from main import db, app
 from sqlalchemy import exc
+import sqlalchemy
 from utils import row2dict
 
 post_blueprint = Blueprint('post', __name__)
 
-
+# TODO: Get the user name from the user table. Figure out the way to communicate between services
 @app.route('/post', methods={'GET'})
 def get_all_post():
-    page_size = request.args.get('post_per_page')
-    page = request.args.get('page')
+    page_size = int(request.args.get('post_per_page'))
+    page = int(request.args.get('page'))
     posts = Post.query.order_by(Post.id)
-    for post in posts:
-        print(post)
     if page is None or page_size is None:
         return jsonify({'code': 200, 'posts': [row2dict(p) for p in posts]})
 
     try:
-        posts = [posts[i: i + int(page_size)] for i in range(0, posts.count(), int(page_size))][int(page)]
-        return jsonify({'code': 200, 'posts': [row2dict(i) for i in posts]})
+        """
+            In this line we do the pagination. 
+            range(0, posts.count() will produce a list from 0 to posts.count() by increment of page_size.
+            We then use those numbers to select the page of posts we want
+        """
+        posts = [posts[i: i + page_size] for i in range(0, posts.count(), page_size)]
+        posts = posts[page]
+        posts = [row2dict(i) for i in posts]
+        for post in posts:
+            post['vote_one'] = Vote.query.filter_by(post_id=post['id'], value=0).count()
+            post['vote_two'] = Vote.query.filter_by(post_id=post['id'], value=1).count()
+
+        return jsonify({'code': 200, 'posts': posts})
     except IndexError:
-        return jsonify({'code': 200, 'end' : True})
+        return jsonify({'code': 200, 'end': True})
 
 
 @app.route('/post/<post_id>', methods={'GET'})
