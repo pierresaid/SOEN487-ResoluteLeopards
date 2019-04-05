d9bc443fe952dadbc1785454c6df2567024b8a9d
Pierre SAID <pierre.said@epitech.eu>
2019-04-04 22:48:56 -0400

    [img_fetch] Added img fetching from imgur api

--- img_fetch_api/main.py
+++ img_fetch_api/main.py
@@ -7,6 +7,8 @@ CORS(app)
 
 catApiUrl = "https://api.thecatapi.com/v1/images/search"
 dogApiUrl = "https://random.dog/woof.json"
+catImgurUrl = "https://api.imgur.com/3/gallery/r/cats/top/month"
+dogImgurUrl = "https://api.imgur.com/3/gallery/r/dogs/top/month"
 
 
 @app.route('/')
@@ -31,6 +33,7 @@ def getRandomCat():
         print("[img_fetch_api]: Error in getRandomCat()")
         return make_response(jsonify({"url": None}), 500)
 
+
 @app.route('/dog')
 def getRandomDog():
     try:
@@ -42,6 +45,32 @@ def getRandomDog():
     except:
         print("[img_fetch_api]: Error in getRandomDog()")
         return make_response(jsonify({"url": None}), 500)
+
+
+@app.route('/imgur/cat/<int:page>', defaults={'page': 0})
+@app.route('/imgur/cat/<int:page>')
+def getRandomCatFromImgur(page):
+    try:
+        response = requests.get(catImgurUrl + '/' + str(page),
+                                headers={'Authorization': 'Client-ID ad9be282186217e'}).json()
+        medias = response['data']
+        images = [m['link'] for m in medias if m['link'].split('.')[-1] == 'jpg']
+        return jsonify({"images": images})
+    except:
+        return make_response(jsonify({"images": []}), 500)
+
+
+@app.route('/imgur/dog/<int:page>', defaults={'page': 0})
+@app.route('/imgur/dog/<int:page>')
+def getRandomDogFromImgur(page):
+    try:
+        response = requests.get(catImgurUrl + '/' + str(page),
+                                headers={'Authorization': 'Client-ID ad9be282186217e'}).json()
+        medias = response['data']
+        images = [m['link'] for m in medias if m['link'].split('.')[-1] == 'jpg']
+        return jsonify({"images": images})
+    except:
+        return make_response(jsonify({"images": []}), 500)
 
 
 if __name__ == '__main__':
