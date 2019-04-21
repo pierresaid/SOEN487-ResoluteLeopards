e09229fe9e7d1dbca24f6a2237dc8d421396b859
Guillaume HITIER <hit1097@gmail.com>
2019-04-20 22:23:42 -0400

    Fixed webapp Authorization header

--- web_app/plugins/axios.js
+++ web_app/plugins/axios.js
@@ -1,5 +1,5 @@
 export default ({ store, app: { $axios } }) => {
   $axios.defaults.headers.common = {
-    Authorization: store.state.user.access_token
+    Authorization: `Bearer ${store.state.user.access_token}`
   }
 }
